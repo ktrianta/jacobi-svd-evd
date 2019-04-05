287fb31ee3ce9095102767811f28ee361ef3bb73
Esref Ozdemir <esref.ozdemir27@gmail.com>
2019-04-05 12:06:35 +0200

    Document debugging utilities

--- src/utils/debug.hpp
+++ src/utils/debug.hpp
@@ -7,16 +7,23 @@
 static const char DEBUG_HEADER[] = "//";
 
 /**
+ * Custom output operator for vector types. With this function the following code becomes possible:
  *
+ * struct vector_t vec;
+ * std::cout << vec << std::endl;
  */
 std::ostream& operator<<(std::ostream& os, const struct vector_t vec);
+
 /**
+ * Custom output operator for matrix types. With this function the following code becomes possible:
  *
+ * struct matrix_t mat;
+ * std::cout << mat << std::endl;
  */
 std::ostream& operator<<(std::ostream& os, const struct matrix_t mat);
 
 /**
- *
+ * Debugging utility base case.
  */
 template <size_t precision = 5, typename T>
 void debug(T t) {
@@ -25,7 +32,11 @@ void debug(T t) {
 }
 
 /**
+ * Debugging utility function. Pass as many types that implement operator<<, and the function will
+ * output each of them to std::cerr, separated by commas.
  *
+ * @tparam precision Precision of floating point values to be printed. Use as debug<P>(your types) where
+ * P is the number of precisions you want, e.g. 10.
  */
 template <size_t precision = 5, typename T, typename... Args>
 void debug(T head, Args... rest) {
