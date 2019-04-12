5cd48175f3b625fa8f6580eaef60ef9cfadf31b1
Konstantinos Triantafyllou <ko.trian@gmail.com>
2019-04-12 18:49:55 +0200

    Add more comments to two-sided svd code

--- src/svd/two-sided/svd.cpp
+++ src/svd/two-sided/svd.cpp
@@ -22,22 +22,25 @@ size_t svd(struct matrix_t Amat, struct matrix_t Bmat, struct matrix_t Umat, str
     double* B = Bmat.ptr;
     double* U = Umat.ptr;
     double* V = Vmat.ptr;
-    double norm = 0.0;      // frobenius norm of matrix
-    double off_norm = 0.0;  // frobenius norm of the off-diagonal elements of matrix
+    double norm = 0.0;      // frobenius norm of matrix B
+    double off_norm = 0.0;  // frobenius norm of the off-diagonal elements of matrix B
 
     matrix_copy(Bmat, Amat);
     matrix_identity(Umat);
     matrix_identity(Vmat);
     matrix_frobenius(Bmat, &norm, &off_norm);
 
+    // Repeat while the frobenius norm of the off-diagonal elements of matrix B, which is updated in every
+    // iteration, is smaller than the forbenius norm of the original matrix B (or A) times the tolerance
     while (off_norm >= tol * norm) {
         for (size_t i = 0; i < n - 1; ++i) {
             for (size_t j = i + 1; j < n; ++j) {
-                const double bii = B[n * i + i];
-                const double bij = B[n * i + j];
-                const double bji = B[n * j + i];
-                const double bjj = B[n * j + j];
+                const double bii = B[n * i + i];  // B[i][i]
+                const double bij = B[n * i + j];  // B[i][j]
+                const double bji = B[n * j + i];  // B[j][i]
+                const double bjj = B[n * j + j];  // B[j][j]
 
+                // Compute the 2x2 svd of B[i][i], B[i][j], B[j][i] and B[j][j]
                 struct svd_2x2_params cf = nsvd(bii, bij, bji, bjj);
 
                 // R_ij(c,s) * B where R_ij(c,s) is the Givens rotation matrix that acts on
