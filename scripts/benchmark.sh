c257eb2f83833b05d987620686320683a969efd7
Konstantinos Triantafyllou <ko.trian@gmail.com>
2019-04-22 14:01:03 +0200

    Refactor one-sided svd performance benchmark
    Now the benchmark expects an array as input instead of having a fixed
    input array. The benchmark script is updated to run the benchmark on
    all available inputs.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
-for bin in bin/benchmark/*; do
+for bin in bin/benchmark/svd/*; do
+    for input in `ls -v perf/resources/svd/*`; do
+        [ -f "$bin" ] && [ -x "$bin" ] && "$bin" < "$input"
+    done
+done
+
+for bin in bin/benchmark/evd/*; do
     [ -f "$bin" ] && [ -x "$bin" ] && "$bin"
 done
