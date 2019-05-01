35259c8c37b8c5519b5c97a3a5e7bdee3320ccb8
Martin Karp <martinkarp@dhcp-77-109-143-165.init7.net>
2019-05-01 15:45:02 +0200

    Fixed performance measure for tol as well

--- perf/evd/cyclic/evd_cyclic_perf.cpp
+++ perf/evd/cyclic/evd_cyclic_perf.cpp
@@ -9,22 +9,6 @@
 using EVDEpochType = decltype(&evd_cyclic);
 using EVDTolType = decltype(&evd_cyclic_tol);
 
-double base_cost_evd(int n, int n_iter) {
-    // The loop_multiplier counts the total number of values
-    // in the upper triangular matrix each corresponding to
-    // an iteration
-    int loop_multiplier = n * (n - 1) * 0.5;
-
-    double add = n_iter * (5 + 6 * n);
-    double mult = n_iter * (7 + 12 * n);
-    double div = n_iter;
-    double sqrt = n_iter * 3;
-
-    return (add + mult + div + sqrt) * loop_multiplier;
-}
-
-using CostFuncType = decltype(&base_cost_evd);
-
 std::vector<EVDEpochType> epoch_based_versions = {
     evd_cyclic,
 };
@@ -39,8 +23,22 @@ std::vector<std::string> tol_based_names = {
     "evd_cyclic_tol",
 };
 
-<<<<<<< HEAD
+double base_cost_evd(size_t n, size_t n_iter) {
+    // The loop_multiplier counts the total number of values
+    // in the upper triangular matrix each corresponding to
+    // an iteration
+    int loop_multiplier = n * (n - 1) * 0.5;
+
+    double add = n_iter * (5 + 6 * n);
+    double mult = n_iter * (7 + 12 * n);
+    double div = n_iter;
+    double sqrt = n_iter * 3;
+
+    return (add + mult + div + sqrt) * loop_multiplier;
+}
+
 double tol_cost(size_t n, size_t n_iter) {
+    // number of upper triangular elements
     double n_elements = n * (n - 1) / 2;
     double adds = (4 + 6 * n) * n_elements + n_elements * 2;
     double muls = (4 + 12 * n) * n_elements + n_elements * 2;
@@ -49,25 +47,17 @@ double tol_cost(size_t n, size_t n_iter) {
 
     return n_iter * (adds + muls + divs + sqrt);
 }
-double base_cost_evd(size_t n, size_t n_iter) {
-    double add = n_iter * (5 + 6 * n);
-    double mult = n_iter * (7 + 12 * n);
-    double div = n_iter;
-    double sqrt = n_iter * 3;
 
-    return add + mult + div + sqrt;
-}
-
+using CostFuncType = decltype(&base_cost_evd);
 using CostFuncType = decltype(&tol_cost);
 
 std::vector<CostFuncType> tol_based_cost_fns = {tol_cost};
-=======
 std::vector<CostFuncType> evdcyclic_epoch_based_cost_fns = {base_cost_evd};
->>>>>>> 52a023ac76ef86fb8857ebb16f0856a0934c3f01
 
 int main() {
     size_t n;
-    size_t n_iter = 20;
+    size_t n_iter = 1000;
+    double tol = 1e-8;
 
     std::ios_base::sync_with_stdio(false);  // disable synchronization between C and C++ standard streams
     std::cin.tie(NULL);                     // untie cin from cout
@@ -84,23 +74,22 @@ int main() {
     vector_t E_vals = {&e[0], n};
     matrix_t E_vecs = {&V[0], n, n};
 
+    for (size_t i = 0; i < n * n; ++i) {
+        std::cin >> A[i];
+    }
+
     std::vector<double> costs;
     for (const auto& cost_fn : evdcyclic_epoch_based_cost_fns) {
         costs.push_back(cost_fn(n, n_iter));
     }
 
-<<<<<<< HEAD
-    size_t n_iter = evd_cyclic_tol(Data_matr, Data_matr_copy, E_vecs, E_vals, 1e-8);
-    std::cout << n_iter;
-    std::vector<double> costs(1, 10000);
+    size_t n_iter_tol = evd_cyclic_tol(Data_matr, Data_matr_copy, E_vecs, E_vals, tol);
     std::vector<double> costs_tol;
 
     for (const auto& cost_fn : tol_based_cost_fns) {
-        costs_tol.push_back(cost_fn(n, n_iter));
+        costs_tol.push_back(cost_fn(n, n_iter_tol));
     }
 
-=======
->>>>>>> 52a023ac76ef86fb8857ebb16f0856a0934c3f01
-    run_all(epoch_based_versions, epoch_based_names, costs, Data_matr, Data_matr_copy, E_vecs, E_vals, 100);
-    run_all(tol_based_versions, tol_based_names, costs_tol, Data_matr, Data_matr_copy, E_vecs, E_vals, 1e-8);
+    run_all(epoch_based_versions, epoch_based_names, costs, Data_matr, Data_matr_copy, E_vecs, E_vals, n_iter);
+    run_all(tol_based_versions, tol_based_names, costs_tol, Data_matr, Data_matr_copy, E_vecs, E_vals, tol);
 }
