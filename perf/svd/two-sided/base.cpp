0c77e1e92ff36a03940744c9f8d967cbc93f61ab
Esref Ozdemir <esref.ozdemir27@gmail.com>
2019-05-11 17:28:04 +0200

    Use new benchmark file structures

--- perf/svd/two-sided/base.cpp
+++ perf/svd/two-sided/base.cpp
@@ -27,15 +27,6 @@ int main() {
         std::cin >> A[i];
     }
 
-    // size_t cost = svd(Data_matr, B_mat, U_mat, V_mat);
-    // bench_func(svd, "svd_two_sided", cost, Data_matr, B_mat, U_mat, V_mat);
-
-    // size_t block_size = 32;
-    // size_t cost = svd_blocked(Data_matr, B_mat, U_mat, V_mat, block_size);
-    // bench_func(svd_blocked, "svd_two_sided_blocked", cost, Data_matr, B_mat, U_mat, V_mat, block_size);
-
-    size_t block_size = 32;
-    size_t cost = svd_blocked_less_copy(Data_matr, B_mat, U_mat, V_mat, block_size);
-    bench_func(svd_blocked_less_copy, "svd_two_sided_blocked_less_copy", cost, Data_matr, B_mat, U_mat, V_mat,
-               block_size);
+    size_t cost = svd(Data_matr, B_mat, U_mat, V_mat);
+    bench_func(svd, "svd_two_sided", cost, Data_matr, B_mat, U_mat, V_mat);
 }
