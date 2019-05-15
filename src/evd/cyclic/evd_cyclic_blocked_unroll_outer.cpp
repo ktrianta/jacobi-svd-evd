4298180ca05544e2122306ce8907874047e979c2
narasimr <narasimr@student.ethz.ch>
2019-05-15 03:50:46 +0200

    Adding missing function declarations

--- src/evd/cyclic/evd_cyclic_blocked_unroll_outer.cpp
+++ src/evd/cyclic/evd_cyclic_blocked_unroll_outer.cpp
@@ -16,6 +16,16 @@ static inline void add(struct matrix_t Amat, struct matrix_t Bmat, struct matrix
 static inline void copy_block(struct matrix_t S, size_t blockS_row, size_t blockS_col, struct matrix_t D,
                               size_t blockD_row, size_t blockD_col, size_t block_size);
 static inline void transpose(struct matrix_t A);
+static inline void mult_add_block(struct matrix_t Amat, size_t blockA_row, size_t blockA_col, struct matrix_t Bmat,
+                    size_t blockB_row, size_t blockB_col, struct matrix_t Cmat, size_t blockC_row, size_t blockC_col,
+                    struct matrix_t Dmat, size_t blockD_row, size_t blockD_col, size_t block_size);
+static inline void mult_transpose_block(struct matrix_t Amat, size_t blockA_row, size_t blockA_col,
+                          struct matrix_t Bmat, size_t blockB_row, size_t blockB_col, struct matrix_t Cmat,
+                          size_t blockC_row, size_t blockC_col, size_t block_size);
+static inline void mult_add_transpose_block(struct matrix_t Amat, size_t blockA_row, size_t blockA_col,
+                          struct matrix_t Bmat, size_t blockB_row, size_t blockB_col, struct matrix_t Cmat,
+                          size_t blockC_row, size_t blockC_col, struct matrix_t Dmat, size_t blockD_row,
+                          size_t blockD_col, size_t block_size);
 
 void evd_cyclic_blocked_unroll_outer(struct matrix_t Data_matr, struct matrix_t Data_matr_copy,
                    struct matrix_t Eigen_vectors, struct vector_t Eigen_values, int epoch) {
@@ -389,8 +399,8 @@ static inline void add(struct matrix_t Amat, struct matrix_t Bmat, struct matrix
     }
 }
 
-static inline void copy_block(struct matrix_t Smat, size_t blockS_row, size_t blockS_col, struct matrix_t Dmat,
-                              size_t blockD_row, size_t blockD_col, size_t block_size) {
+static inline void copy_block(struct matrix_t Smat, size_t blockS_row, size_t blockS_col,
+                      struct matrix_t Dmat, size_t blockD_row, size_t blockD_col, size_t block_size) {
     size_t nS = Smat.rows;
     size_t nD = Dmat.rows;
     double* S = Smat.ptr;
@@ -398,8 +408,15 @@ static inline void copy_block(struct matrix_t Smat, size_t blockS_row, size_t bl
     size_t Sbeg = blockS_row * block_size * nS + blockS_col * block_size;
     size_t Dbeg = blockD_row * block_size * nD + blockD_col * block_size;
     for (size_t i = 0; i < block_size; ++i) {
-        for (size_t j = 0; j < block_size; ++j) {
-            D[Dbeg + i * nD + j] = S[Sbeg + i * nS + j];
+        for (size_t j = 0; j < block_size; j += 8) {
+            D[Dbeg + i * nD + j + 0] = S[Sbeg + i * nS + j + 0];
+            D[Dbeg + i * nD + j + 1] = S[Sbeg + i * nS + j + 1];
+            D[Dbeg + i * nD + j + 2] = S[Sbeg + i * nS + j + 2];
+            D[Dbeg + i * nD + j + 3] = S[Sbeg + i * nS + j + 3];
+            D[Dbeg + i * nD + j + 4] = S[Sbeg + i * nS + j + 4];
+            D[Dbeg + i * nD + j + 5] = S[Sbeg + i * nS + j + 5];
+            D[Dbeg + i * nD + j + 6] = S[Sbeg + i * nS + j + 6];
+            D[Dbeg + i * nD + j + 7] = S[Sbeg + i * nS + j + 7];
         }
     }
 }
@@ -412,6 +429,84 @@ static inline void transpose(struct matrix_t Amat) {
             double tmp = A[i * n + j];
             A[i * n + j] = A[j * n + i];
             A[j * n + i] = tmp;
+        }
+    }
+}
+
+// perform D = C + AB
+static inline void mult_add_block(struct matrix_t Amat, size_t blockA_row, size_t blockA_col, struct matrix_t Bmat,
+              size_t blockB_row, size_t blockB_col, struct matrix_t Cmat, size_t blockC_row, size_t blockC_col,
+              struct matrix_t Dmat, size_t blockD_row, size_t blockD_col, size_t block_size) {
+    size_t nA = Amat.rows;
+    size_t nB = Bmat.rows;
+    size_t nC = Cmat.rows;
+    size_t nD = Dmat.rows;
+    double* A = Amat.ptr;
+    double* B = Bmat.ptr;
+    double* C = Cmat.ptr;
+    double* D = Dmat.ptr;
+    size_t Abeg = blockA_row * block_size * nA + blockA_col * block_size;
+    size_t Bbeg = blockB_row * block_size * nB + blockB_col * block_size;
+    size_t Cbeg = blockC_row * block_size * nC + blockC_col * block_size;
+    size_t Dbeg = blockD_row * block_size * nD + blockD_col * block_size;
+    for (size_t i = 0; i < block_size; ++i) {
+        for (size_t j = 0; j < block_size; ++j) {
+            D[Dbeg + i * nD + j] = C[Cbeg + i * nC + j];
+            for (size_t k = 0; k < block_size; ++k) {
+                D[Dbeg + i * nD + j] += A[Abeg + i * nA + k] * B[Bbeg + k * nB + j];
+            }
+        }
+    }
+}
+
+// perform C += (A^T)B
+// for C_ij, use ith column of A and jth column of B
+void inline mult_transpose_block(struct matrix_t Amat, size_t blockA_row, size_t blockA_col, struct matrix_t Bmat,
+                          size_t blockB_row, size_t blockB_col, struct matrix_t Cmat, size_t blockC_row,
+                          size_t blockC_col, size_t block_size) {
+    size_t nA = Amat.rows;
+    size_t nB = Bmat.rows;
+    size_t nC = Cmat.rows;
+    double* A = Amat.ptr;
+    double* B = Bmat.ptr;
+    double* C = Cmat.ptr;
+    size_t Abeg = blockA_row * block_size * nA + blockA_col * block_size;
+    size_t Bbeg = blockB_row * block_size * nB + blockB_col * block_size;
+    size_t Cbeg = blockC_row * block_size * nC + blockC_col * block_size;
+    for (size_t i = 0; i < block_size; ++i) {
+        for (size_t j = 0; j < block_size; ++j) {
+            C[Cbeg + i * nC + j] = 0.0;
+            for (size_t k = 0; k < block_size; ++k) {
+                C[Cbeg + i * nC + j] += A[Abeg + k * nA + i] * B[Bbeg + k * nB + j];
+            }
+        }
+    }
+}
+
+// perform D = C + (A^T)B
+// for D_ij, use ith column of A and jth column of B.
+void inline mult_add_transpose_block(struct matrix_t Amat, size_t blockA_row, size_t blockA_col, struct matrix_t Bmat,
+                              size_t blockB_row, size_t blockB_col, struct matrix_t Cmat, size_t blockC_row,
+                              size_t blockC_col, struct matrix_t Dmat, size_t blockD_row, size_t blockD_col,
+                              size_t block_size) {
+    size_t nA = Amat.rows;
+    size_t nB = Bmat.rows;
+    size_t nC = Cmat.rows;
+    size_t nD = Dmat.rows;
+    double* A = Amat.ptr;
+    double* B = Bmat.ptr;
+    double* C = Cmat.ptr;
+    double* D = Dmat.ptr;
+    size_t Abeg = blockA_row * block_size * nA + blockA_col * block_size;
+    size_t Bbeg = blockB_row * block_size * nB + blockB_col * block_size;
+    size_t Cbeg = blockC_row * block_size * nC + blockC_col * block_size;
+    size_t Dbeg = blockD_row * block_size * nD + blockD_col * block_size;
+    for (size_t i = 0; i < block_size; ++i) {
+        for (size_t j = 0; j < block_size; ++j) {
+            D[Dbeg + i * nD + j] = C[Cbeg + i * nC + j];
+            for (size_t k = 0; k < block_size; ++k) {
+                D[Dbeg + i * nD + j] += A[Abeg + k * nA + i] * B[Bbeg + k * nB + j];
+            }
         }
     }
 }
