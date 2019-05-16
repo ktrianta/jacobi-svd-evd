e7e99332e875c7183dd0b24505685c748d38a9c0
Martin Karp <martinkarp@localhost.localdomain>
2019-05-16 11:29:21 +0200

    Added tests in indivudal files.

--- test/evd/cyclic/evd_cyclic_oneloop_test.cpp
+++ test/evd/cyclic/evd_cyclic_oneloop_test.cpp
@@ -1,13 +1,13 @@
-#include "evd_cyclic.hpp"
 #include <math.h>
 #include <iostream>
 #include <random>
 #include <vector>
 #include "../../test_utils.hpp"
+#include "evd_cyclic.hpp"
 #include "gtest/gtest.h"
 #include "types.hpp"
 
-TEST(evd_cyclic, identity_matrix) {
+TEST(evd_cyclic_oneloop, identity_matrix) {
     size_t n = 10;
     std::vector<double> A(n * n, 0);
     std::vector<double> A_copy(n * n, 0);
@@ -21,13 +21,13 @@ TEST(evd_cyclic, identity_matrix) {
     vector_t E_vals = {&e[0], n};
     matrix_t E_vecs = {&V[0], n, n};
 
-    evd_cyclic(Data_matr, Data_matr_copy, E_vecs, E_vals, 100);
+    evd_cyclic_oneloop(Data_matr, Data_matr_copy, E_vecs, E_vals, 100);
     for (size_t i = 0; i < n; ++i) {
         ASSERT_DOUBLE_EQ(e[i], 1.0);
     }
 }
 
-TEST(evd_cyclic, random_square_matrix) {
+TEST(evd_cyclic_oneloop, random_square_matrix) {
     size_t n = 4;
     std::vector<double> A = {7.0, 3.0, 2.0, 1.0, 3.0, 9.0, -2.0, 4.0, 2.0, -2.0, -4.0, 2.0, 1.0, 4.0, 2.0, 3.0};
     std::vector<double> A_copy(n * n, 0);
@@ -39,7 +39,7 @@ TEST(evd_cyclic, random_square_matrix) {
     vector_t E_vals = {&e[0], n};
     matrix_t E_vecs = {&V[0], n, n};
 
-    evd_cyclic(Data_matr, Data_matr_copy, E_vecs, E_vals, 100);
+    evd_cyclic_oneloop(Data_matr, Data_matr_copy, E_vecs, E_vals, 100);
 
     std::vector<double> e_expect = {12.71986, 5.78305, 2.09733, -5.60024};
 
@@ -48,7 +48,7 @@ TEST(evd_cyclic, random_square_matrix) {
     }
 }
 
-TEST(evd_cyclic, evd_crosscheck) {
+TEST(evd_cyclic_oneloop, evd_crosscheck) {
     size_t n = 5;
     std::vector<double> A = {
         2.000000000000000000e+00, 6.000000000000000000e+00, 4.000000000000000000e+00, 6.000000000000000000e+00,
@@ -67,17 +67,18 @@ TEST(evd_cyclic, evd_crosscheck) {
     vector_t E_vals = {&e[0], n};
     matrix_t E_vecs = {&V[0], n, n};
 
-    evd_cyclic(Data_matr, Data_matr_copy, E_vecs, E_vals, 100);
+    evd_cyclic_oneloop(Data_matr, Data_matr_copy, E_vecs, E_vals, 100);
 
     std::vector<double> e_expect = {2.415032147975995969e+01, 4.001355036163166012e+00, -1.007738346679503572e+00,
                                     -3.262428878677021693e+00, -5.881509290566617310e+00};
+    evd_cyclic(Data_matr, Data_matr_copy, E_vecs, E_vals, 100);
 
     for (size_t i = 0; i < n; ++i) {
         ASSERT_NEAR(e[i], e_expect[i], 1e-7);
     }
 }
 
-TEST(evd_cyclic, eigenvector_check) {
+TEST(evd_cyclic_oneloop, eigenvector_check) {
     size_t n = 5;
     std::vector<double> A = {
         2.000000000000000000e+00, 6.000000000000000000e+00, 4.000000000000000000e+00, 6.000000000000000000e+00,
@@ -96,7 +97,7 @@ TEST(evd_cyclic, eigenvector_check) {
     vector_t E_vals = {&e[0], n};
     matrix_t E_vecs = {&V[0], n, n};
 
-    evd_cyclic(Data_matr, Data_matr_copy, E_vecs, E_vals, 100);
+    evd_cyclic_oneloop(Data_matr, Data_matr_copy, E_vecs, E_vals, 100);
 
     std::vector<double> V_expect = {
         4.183471639051151714e-01,  4.111245337904025771e-02,  7.089492553079320691e-01,  -2.465472573146875179e-01,
@@ -111,11 +112,17 @@ TEST(evd_cyclic, eigenvector_check) {
         int sign = (V[j] / V_expect[j] < 0) ? -1 : 1;
         for (size_t i = 0; i < n; ++i) {
             ASSERT_NEAR(sign * V[n * i + j], V_expect[n * i + j], 1e-7);
+            for (size_t j = 0; j < n; ++j) {
+                // equal up to sign
+                int sign = (V[j] / V_expect[j] < 0) ? -1 : 1;
+                for (size_t i = 0; i < n; ++i) {
+                    ASSERT_NEAR(sign * V[i * n + j], V_expect[i * n + j], 1e-7);
+                }
+            }
         }
     }
 }
-
-TEST(evd_cyclic, random_matrix_big) {
+TEST(evd_cyclic_oneloop, random_matrix_big) {
     size_t n = 128;
 
     std::vector<double> A(n * n, 0);
@@ -133,7 +140,7 @@ TEST(evd_cyclic, random_matrix_big) {
     matrix_t Data_matr_copy = {&A_copy[0], n, n};
     vector_t E_vals = {&e[0], n};
     matrix_t E_vecs = {&V[0], n, n};
-    evd_cyclic(Data_matr, Data_matr_copy, E_vecs, E_vals, 100);
+    evd_cyclic_oneloop(Data_matr, Data_matr_copy, E_vecs, E_vals, 100);
 
     for (size_t i = 0; i < n; ++i) {
         ASSERT_NEAR(e[i], e_expect[i], 1e-7);
