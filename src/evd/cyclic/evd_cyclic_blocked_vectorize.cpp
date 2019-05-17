db17d53a0ea34ace90acde43ce592a106c0279cc
narasimr <narasimr@student.ethz.ch>
2019-05-17 11:16:07 +0200

    Lint issues..

--- src/evd/cyclic/evd_cyclic_blocked_vectorize.cpp
+++ src/evd/cyclic/evd_cyclic_blocked_vectorize.cpp
@@ -91,7 +91,7 @@ void evd_cyclic_blocked_vectorize(struct matrix_t Data_matr, struct matrix_t Dat
                 copy_block(Amat, j_block, j_block, Ablockmat, 1, 1, block_size);
 
                 evd_block_vector(Ablockmat, Vblockmat);
-                //evd_subprocedure_vectorized(Ablockmat, Vblockmat);
+                // evd_subprocedure_vectorized(Ablockmat, Vblockmat);
 
                 transpose(Vblockmat);
 
@@ -203,7 +203,7 @@ void evd_cyclic_blocked_less_copy_vectorize(struct matrix_t Data_matr, struct ma
                 copy_block(Amat, j_block, j_block, Ablockmat, 1, 1, block_size);
 
                 evd_block_vector(Ablockmat, Vblockmat);
-                //evd_subprocedure_vectorized(Ablockmat, Vblockmat);
+                // evd_subprocedure_vectorized(Ablockmat, Vblockmat);
 
                 for (size_t k_block = 0; k_block < n_blocks; ++k_block) {
                     mult_transpose_block(Vblockmat, 0, 0, Amat, i_block, k_block, M1mat, 0, 0, block_size);
