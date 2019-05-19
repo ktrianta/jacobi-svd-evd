0e20864acce79d3f35c5c0b97982d8e3a5965b27
narasimr <narasimr@student.ethz.ch>
2019-05-20 00:50:47 +0200

    Adding cost functions for EVD

---
+++ src/evd/cyclic/evd_cost.cpp
@@ -0,0 +1,80 @@
+#include "evd_cost.hpp"
+#include <cstddef>
+
+size_t base_cost_evd(size_t n, size_t n_iter) {
+
+    size_t loops = n * (n - 1) * 0.5;
+
+    size_t rot_add = 6 * loops;
+    size_t evd_add = 6 * n * loops;
+    size_t add = n_iter * (rot_add + evd_add);
+
+    size_t rot_mul = 5 * loops;
+    size_t evd_mul = 12 * n * loops;
+    size_t mul = n_iter * (rot_mul + evd_mul);
+
+    //These two are only used in sym_jacobi_coeffs with mutually exclusive condition
+    size_t div = n_iter * (3 * loops);
+    size_t sqrt = n_iter * (2 * loops);
+
+    return add + mul + div + sqrt;
+}
+
+double tol_cost(size_t n, size_t n_iter) {
+    double adds = 4 + 6 * n + n * (n - 1);
+    double muls = 3 + 12 * n + n * (n - 1);
+    double divs = 3;
+    double sqrt = 2;
+
+    return n_iter * (adds + muls + divs + sqrt);
+}
+
+size_t base_cost_vectorized_evd(size_t n, size_t n_iter) {
+    size_t loops = n * (n - 1) * 0.5;
+
+    size_t rot_add = 6 * loops;
+    size_t evd_add = 6 * n * loops;
+    size_t add = n_iter * (rot_add + evd_add);
+
+    size_t rot_mul = 5 * loops;
+    size_t evd_mul = 12 * n * loops;
+    size_t mul = n_iter * (rot_mul + evd_mul);
+
+    //These two are only used in sym_jacobi_coeffs with mutually exclusive condition
+    size_t div = n_iter * (3 * loops);
+    size_t sqrt = n_iter * (2 * loops);
+
+    return add + mul + div + sqrt;
+}
+
+size_t blocked_cost_without_subprocedure_evd(size_t n, size_t b, size_t n_iter, size_t individual_block_iter) {
+    size_t n_blocks = n / b;
+    size_t loops = n_blocks * (n_blocks - 1) / 2;
+
+    size_t single_mult_block_add = b * b * b;
+    size_t single_mult_block_mul = b * b * b;
+    size_t single_add = b * b;
+
+    size_t block_adds = loops * (4 * n_blocks * (12 * single_mult_block_add + 6 * single_add));
+    size_t block_muls = loops * (4 * n_blocks * (12 * single_mult_block_mul));
+    size_t individual_block_ops = loops * base_cost_evd(4 * b, individual_block_iter); // 4 individual blocks
+    size_t total_ops_per_iter = (block_adds + block_muls + individual_block_ops);
+
+    return n_iter * total_ops_per_iter;
+}
+
+size_t blocked_less_copy_cost_without_subprocedure_evd(size_t n, size_t b, size_t n_iter,
+                                                       size_t individual_block_iter) {
+    size_t n_blocks = n / b;
+    size_t loops = n_blocks * (n_blocks - 1) / 2;
+
+    size_t single_mult_block_add = b * b * b;
+    size_t single_mult_block_mul = b * b * b;
+
+    size_t block_adds = loops * (4 * n_blocks * (12 * single_mult_block_add));
+    size_t block_muls = loops * (4 * n_blocks * (12 * single_mult_block_mul));
+    size_t individual_block_ops = loops * base_cost_evd(4 * b, individual_block_iter); // 4 individual blocks
+    size_t total_ops_per_iter = (block_adds + block_muls + individual_block_ops);
+
+    return n_iter * total_ops_per_iter;
+}
